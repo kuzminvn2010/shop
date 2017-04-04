fddd4f60e54d61aa17f2943fc4bd1cdbf31f6648
kuzminvn2010 <kuzminvn2010@yandex.ru>
2017-04-04 05:36:12 +0300

    added class ProductCollection

---
+++ lib/productcollection.rb
@@ -0,0 +1,48 @@
+class ProductCollection
+
+  def self.from_dir(current_path)
+    products = []
+    films_paths = Dir.glob("#{current_path}/data/films/*.txt")
+    # записываем в массив  products все экземпляры класса Film
+    films_paths.each do |file_path|
+      products << Film.from_file(file_path)
+    end
+
+    books_paths = Dir.glob("#{current_path}/data/books/*.txt")
+    books_paths.each do |file_path|
+      products << Book.from_file(file_path)
+    end
+
+    self.new(products)
+  end
+
+  def initialize(products)
+    @products = products
+  end
+
+  def to_a
+    @products
+  end
+
+  def sort!(params)
+    # Делает выбор по параметру by
+    case params[:by]
+      when :title
+        # Если запросили сортировку по наименованию
+        @products.sort_by! { |product| product.to_s }
+      when :price
+        # Если запросили сортировку по цене
+        @products.sort_by! { |product| product.price }
+      when :quantity
+        # Если запросили сортировку по количеству
+        @products.sort_by! { |product| product.quantity }
+    end
+
+    # Если запросили сортировку по убыванию
+    @products.reverse! if params[:order] == :decrease
+
+    # Возвращаем ссылку на экземпляр, чтобы у него по цепочке можно было вызвать
+    # другие методы.
+    self
+  end
+end
--- main.rb
+++ main.rb
@@ -1,31 +1,25 @@
 require_relative "lib/product"
 require_relative "lib/film"
 require_relative "lib/book"
+require_relative "lib/productcollection"
 
 current_path = File.dirname(__FILE__)
+collections = ProductCollection.from_dir(current_path)
 
-films_paths = Dir.glob("#{current_path}/data/films/*.txt").sort!
-films = []
-# записываем в массив  clothes все экземпляры класса ClothesItem
-films_paths.each do |file_path|
-  films << Film.from_file(file_path)
-end
-puts "Фильмы:\n"
-films.each do |film|
-  puts film.to_s
-end
-puts
+puts collections.sort!(:by => :title).to_a
 
-books_paths = Dir.glob("#{current_path}/data/books/*.txt").sort!
-books = []
-# записываем в массив  clothes все экземпляры класса ClothesItem
-books_paths.each do |file_path|
-  books << Book.from_file(file_path)
-end
-puts "Книги:\n"
-books.each do |book|
-  puts book.to_s
-end
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #film = Product.from_file("#{current_path}/data/films/01.txt")
