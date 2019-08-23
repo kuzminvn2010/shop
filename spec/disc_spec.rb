5cbd74336d396f2ea37744bd21bdcf7e4b1de874
Vadim <kuzminvn2010@yandex.ru>
2019-08-23 14:43:03 +0400

    modify discspec

--- spec/disc_spec.rb
+++ spec/disc_spec.rb
@@ -2,18 +2,18 @@ require 'rspec'
 require_relative '../lib/product'
 require_relative '../lib/disc'
 
-describe 'Disc' do
-  file_path = File.dirname(__FILE__) + '/fixtures/1.txt'
-  disc = Disc.from_file(file_path)
+describe 'Disc initialize' do
+  let (:file_path) { File.dirname(__FILE__) + '/fixtures/1.txt'}
+  let (:disc) {Disc.from_file(file_path) }
 
-  context 'disc methods' do
-    it "check disc init attributes" do
-       expect(disc).to have_attributes(title: 'Painkiller')
-       expect(disc).to have_attributes(author: 'Judas Priest')
-       expect(disc).to have_attributes(genre: 'Heavy Metal')
-       expect(disc).to have_attributes(year: '1990'.to_i )
-       expect(disc).to have_attributes(price: '990'.to_i )
-       expect(disc).to have_attributes(quantity: '3'.to_i )
+    it "checks disc init attributes" do
+      expect(disc).to have_attributes(
+        title: 'Painkiller',
+        author: 'Judas Priest',
+        genre: 'Heavy Metal',
+        year: '1990'.to_i,
+        price: '990'.to_i,
+        quantity: '3'.to_i
+        )
     end
-  end
 end
